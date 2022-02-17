57c142e3640bafa1123ca3fca6ff72a03e498bef
Jeffrey Evans <jeffrey_evans@tnc.org>
2022-02-17 15:18:12 -0700

    Update EcoEcon_NeighborEmbedding.R

--- R/EcoEcon_NeighborEmbedding.R
+++ R/EcoEcon_NeighborEmbedding.R
@@ -113,78 +113,13 @@ ggplot(edat, aes(x = X1, y = X2, color = realm)) +
           theme_bw()
 	  
 
-# Create interactive plot
-  fig <- plot_ly(edat, x = ~X1, y = ~X2, split = ~label,
-                 type = 'scatter', mode = 'markers', colors=clr) %>%  
-  layout(  
-    plot_bgcolor = "#e5ecf6",
-    legend = list(title = list(text = "Ecoregions")),
-    xaxis = list(title = "0"),  
-    yaxis = list(title = "1")) 
-
-fig
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-plot.umap <- function(x, labels,
-         main="UMAP embeddings",
-         colors=c("#ff7f00", "#e377c2", "#17becf"),
-         pad=0.1, cex=0.6, pch=19, add=FALSE, legend.suffix="",
-         cex.main=1, cex.legend=0.85) {
-  if(class(x) != "umap")
-    stop("x is not a umap embedding object")
-     ulayout = x$layout
-  xylim = range(ulayout)
-  xylim = xylim + ((xylim[2]-xylim[1])*pad)*c(-0.5, 0.5)
-  if (!add) {
-    par(mar=c(0.2,0.7,1.2,0.7), ps=10)
-      plot(xylim, xylim, type="n", axes=F, frame=F)
-    rect(xylim[1], xylim[1], xylim[2], xylim[2], border="#aaaaaa", lwd=0.25)  
-  }
-  points(ulayout[,1], ulayout[,2], col=colors[as.integer(labels)],
-         cex=cex, pch=pch)
-  mtext(side=3, main, cex=cex.main)
-
-  labels.u = unique(labels)
-  legend.pos = "topleft"
-  legend.text = as.character(labels.u)
-  if (add) {
-    legend.pos = "bottomleft"
-    legend.text = paste(as.character(labels.u), legend.suffix)
-  }
-
-  legend(legend.pos, legend=legend.text, inset=0.03,
-         col=colors[as.integer(labels.u)],
-         bty="n", pch=pch, cex=cex.legend)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# # Create interactive plot
+#   fig <- plot_ly(edat, x = ~X1, y = ~X2, split = ~label,
+#                  type = 'scatter', mode = 'markers', colors=clr) %>%  
+#   layout(  
+#     plot_bgcolor = "#e5ecf6",
+#     legend = list(title = list(text = "Ecoregions")),
+#     xaxis = list(title = "0"),  
+#     yaxis = list(title = "1")) 
+# 
+# fig
